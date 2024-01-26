cd04857a7b34588aaead0e23c7cddbb4df8331b1
Jemi Yosua Laoere <jemiyosua@gmail.com>
2024-01-27 03:19:44 +0700

    update design cek ktp berhasil (data ditemukan)

--- detail-data-ptps.php
+++ detail-data-ptps.php
@@ -18,7 +18,16 @@ if (isset($_SESSION['nomor_ktp'])) {
     $Kecamatan = $row["kecamatan"];
     $Kelurahan = $row["kelurahan"];
     $NomorTPS = $row["no_tps"];
+    $NomorTPS2 = "";
+    echo strlen($NomorTPS);
+    if (strlen($NomorTPS) == 2) {
+        $NomorTPS2 = "0" . $NomorTPS;
+    } else {
+        $NomorTPS2 = "00" . $NomorTPS;
+    }
     $Nama = $row["nama"];
+    $NIKSubString = substr($NomorKTP, 0, 6);
+    $NIK = $NIKSubString . "**********";
 }
 
 
@@ -30,14 +39,35 @@ if (isset($_SESSION['nomor_ktp'])) {
         <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                    <div class="col-lg-6 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
-                        <div class="card">
-                            <h5 class="card-header">Detail Data PTPS</h5>
+                        <div class="card" style="width: 20rem;">
+                            <div style="padding:10px;text-align:center;margin-top:20px">
+                                <img src="assets/img/bawaslu/verify.png" style="width: 75px;" class="card-img-top" alt="...">
+                            </div>
                             <div class="card-body">
-                                <h5 class="card-title"><?= $Nama ?></h5>
-                                <p class="card-text"></p>
-                                <a href="#" class="btn btn-primary"></a>
+                                <h5 class="card-title" style="text-align:center;font-size: 25px;"><?= $Nama ?></h5>
+                                <h5 style="text-align:center;font-size: 20px;"><?= $NIK ?></h5>
+                                <hr />
+                                <div class="row">
+                                    <div class="col-sm-6 mb-6 mb-sm-0">
+                                        <div>
+                                            <div class="card-text">Kecamatan</div>
+                                            <div class="card-text" style="font-weight: bold;"><?= $Kecamatan ?></div>
+                                            <br/>
+                                            <div class="card-text">Kelurahan</div>
+                                            <div class="card-text" style="font-weight: bold;"><?= $Kelurahan ?></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6 mb-6 mb-sm-0">
+                                        <div style="text-align: right;">TPS / <text style="font-weight: bold;">DPT</text></div>
+                                        <h1 style="font-weight: bold;text-align: right;"><?= $NomorTPS2 ?></h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+                            <div class="card-body">
+                                <a href="index.php" class="btn btn-success w-100">Next</a>
                             </div>
                         </div>
 
