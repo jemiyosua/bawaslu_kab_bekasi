468accf9cbe898b96d37e7e4c060a7adce7cb9b3
Jemi Yosua Laoere <jemiyosua@gmail.com>
2024-01-27 11:29:21 +0700

    update koneksi ke server

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$database = "bawaslu";
-$username = "root";
-$password = "";
+$servername = "srv1156.hstgr.io";
+$database = "u189988533_bawaslu";
+$username = "u189988533_bawaslu";
+$password = "Bawaslu123!";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
